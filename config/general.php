81d50538cd9a0d972f473409d6f237f756fb518f
Liam Rella <Liam.Rella@gmail.com>
2019-12-02 14:12:41 +1030

    install craft

--- config/general.php
+++ config/general.php
@@ -9,40 +9,34 @@
  */
 
 return [
-    // Global settings
-    '*' => [
-        // Default Week Start Day (0 = Sunday, 1 = Monday...)
-        'defaultWeekStartDay' => 1,
-
-        // Whether generated URLs should omit "index.php"
-        'omitScriptNameInUrls' => true,
-
-        // Control Panel trigger word
-        'cpTrigger' => 'admin',
-
-        // The secure key Craft will use for hashing and encrypting data
-        'securityKey' => getenv('SECURITY_KEY'),
-
-        // Whether to save the project config out to config/project.yaml
-        // (see https://docs.craftcms.com/v3/project-config.html)
-        'useProjectConfigFile' => false,
-    ],
-
-    // Dev environment settings
-    'dev' => [
-        // Dev Mode (see https://craftcms.com/guides/what-dev-mode-does)
-        'devMode' => true,
-    ],
-
-    // Staging environment settings
-    'staging' => [
-        // Set this to `false` to prevent administrative changes from being made on staging
-        'allowAdminChanges' => true,
-    ],
-
-    // Production environment settings
-    'production' => [
-        // Set this to `false` to prevent administrative changes from being made on production
-        'allowAdminChanges' => true,
-    ],
+  // Craft config settings from .env variables
+  'aliases' => [
+    '@assetsUrl' => getenv('ASSETS_URL'),
+    '@web' => getenv('SITE_URL'),
+    '@webroot' => getenv('WEB_ROOT_PATH'),
+  ],
+  'allowUpdates' => (bool)getenv('ALLOW_UPDATES'),
+  'allowAdminChanges' => (bool)getenv('ALLOW_ADMIN_CHANGES'),
+  'backupOnUpdate' => (bool)getenv('BACKUP_ON_UPDATE'),
+  'cpTrigger' => getenv('CP_TRIGGER') ?: 'admin',
+  'devMode' => (bool)getenv('DEV_MODE'),
+  'enableTemplateCaching' => (bool)getenv('ENABLE_TEMPLATE_CACHING'),
+  'isSystemLive' => (bool)getenv('IS_SYSTEM_LIVE'),
+  'resourceBasePath' => dirname(__DIR__) . '/web/cpresources',
+  'securityKey' => getenv('SECURITY_KEY'),
+  // Craft config settings from constants
+  'cacheDuration' => false,
+  'defaultSearchTermOptions' => [
+    'subLeft' => true,
+    'subRight' => true,
+  ],
+  'enableCsrfProtection' => true,
+  'errorTemplatePrefix' => 'errors/',
+  'generateTransformsBeforePageLoad' => true,
+  'maxCachedCloudImageSize' => 3000,
+  'maxRevisions' => 5,
+  'maxUploadFileSize' => '100M',
+  'omitScriptNameInUrls' => true,
+  'useProjectConfigFile' => true,
+  'useEmailAsUsername' => true,
 ];
