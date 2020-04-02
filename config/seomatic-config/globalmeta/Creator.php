40137c73b74340111dc9b8afd8d02b65fe5c868e
Liam Rella <Liam.Rella@gmail.com>
2020-04-02 15:10:02 +1030

    add seomatic preconfig

---
+++ config/seomatic-config/globalmeta/Creator.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * SEOmatic plugin for Craft CMS 3.x
+ *
+ * A turnkey SEO implementation for Craft CMS that is comprehensive, powerful,
+ * and flexible
+ *
+ * @link      https://nystudio107.com
+ * @copyright Copyright (c) 2017 nystudio107
+ */
+
+/**
+ * @author    nystudio107
+ * @package   Seomatic
+ * @since     3.0.0
+ */
+
+return [
+    '*' => [
+        'siteType'                     => 'Organization',
+        'siteSubType'                  => 'GovernmentOrganization',
+        'siteSpecificType'             => '',
+        'computedType'                 => 'Organization',
+        'genericName'                  => 'Department for Environment and Water',
+        'genericAlternateName'         => 'DEW',
+        'genericDescription'           => 'The Department for Environment and Water aims to help South Australians conserve, sustain and prosper.',
+        'genericUrl'                   => 'https://environment.sa.gov.au/',
+        'genericImage'                 => 'https://d3u55zae1ecw4x.cloudfront.net/shared/images/Logos/gosa-logotype-color.svg',
+        'genericImageWidth'            => '146',
+        'genericImageHeight'           => '40.75',
+        'genericImageIds'              => [],
+        'genericTelephone'             => '',
+        'genericEmail'                 => 'dew.internet@sa.gov.au',
+        'genericStreetAddress'         => '81 Waymouth Street',
+        'genericAddressLocality'       => 'Adelaide',
+        'genericAddressRegion'         => 'SA',
+        'genericPostalCode'            => '5000',
+        'genericAddressCountry'        => 'AU',
+        'genericGeoLatitude'           => '43.2365384',
+        'genericGeoLongitude'          => '-77.49211620000001',
+        'personGender'                 => '',
+        'personBirthPlace'             => '',
+        'organizationDuns'             => '',
+        'organizationFounder'          => '',
+        'organizationFoundingDate'     => '',
+        'organizationFoundingLocation' => 'Adelaide, South Australia',
+        'organizationContactPoints'    => [],
+        'corporationTickerSymbol'      => '',
+        'localBusinessPriceRange'      => '',
+        'localBusinessOpeningHours'    => [],
+        'restaurantServesCuisine'      => '',
+        'restaurantMenuUrl'            => '',
+        'restaurantReservationsUrl'    => '',
+    ],
+];
