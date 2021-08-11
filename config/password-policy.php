cec836d5f38bb83ce34d8843bf32cf008c3db6cf
mina <gogo.mina@gmail.com>
2021-08-11 15:37:36 +0930

    Add a config file to set password rules

---
+++ config/password-policy.php
@@ -0,0 +1,23 @@
+<?php
+
+use craft\helpers\App;
+
+return [
+    // Minimum password length
+    "minLength" => 10,
+    
+    // Maximum password length
+    "maxLength" => 160,
+    
+    // Force users to use different cases
+    "cases" => 1,
+    
+    // Require at least 1 number
+    "numbers" => 1,
+    
+    // Require at least one symbol
+    "symbols" => 1,
+    
+    // Show a password strength indicator
+    "showStrengthIndicator" => 1,
+];
